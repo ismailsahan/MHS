de5d01975e3b390543c83ddd00065762b0c179ec
gwc0721 <gwc0721@qq.com>
2013-12-04 17:01:37 +0800

    v0.1.1 更新Github项目配置文件和删除缓存

--- cache/1b0994dc379d5b70dd20fe40fd44498a_tpl.php
+++
@@ -1,4 +0,0 @@
-<?php if(!$this || is_object($this) === false){exit('Hacking!');} ?>	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script><![endif]-->
-<!-- END BODY -->
-</body>
-</html>
