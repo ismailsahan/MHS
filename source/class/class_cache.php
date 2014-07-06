37db04f4ab0e1fc4302e261e51f316cd3f7d8937
gwc0721 <gwc0721@qq.com>
2014-07-06 23:54:14 +0800

    修复phpFastCache无法建立特定文件夹的问题

--- source/class/class_cache.php
+++ source/class/class_cache.php
@@ -31,9 +31,10 @@ class Cache {
 	public static function init() {
 		global $_G;
 		phpFastCache::$config['path'] = APP_FRAMEWORK_ROOT.'/data/cache';
+		phpFastCache::$config['securityKey'] = md5($_SERVER['HTTP_HOST'].$_SERVER['SERVER_PORT'].$_G['siteroot']);
 		self::$prefix = $_G['config']['cookie']['cookiepre'];
 		self::$object = new phpFastCache('auto');
-		self::$object->option('path', APP_FRAMEWORK_ROOT.'/data/cache');
+		//self::$object->option('path', APP_FRAMEWORK_ROOT.'/data/cache');
 		self::$storage = ucfirst(self::$object->option['storage']);
 		return self::$object;
 	}
