29373ea798bc6010d3e2e6ea7e765dd11deff655
gwc0721 <gwc0721@qq.com>
2014-08-17 19:13:07 +0800

    Fix page styles and add cronjob class

---
+++ source/class/class_cron.php
@@ -0,0 +1,18 @@
+<?php
+
+class Cron {
+
+	/**
+	 * 主动断开与客户端的HTTP连接
+	 */
+	public function closeconnection() {
+		if(headers_sent()) return false;
+		echo time();
+		$size = ob_get_length();
+		header("Content-Length: {$size}");
+		header('Connection: Close');
+		ob_flush();
+		return true;
+	}
+
+}
